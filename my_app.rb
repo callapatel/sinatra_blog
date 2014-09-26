d83801fcabdada770a88cae4e6290fd7cd2e4030
Calla Patel <callapatel@gmail.com>
2014-09-26 13:55:36 -0700

    lots of pushes, images, style, about, index, posts

--- my_app.rb
+++ my_app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
+require './lib/post'
 
 class MyApp < Sinatra::Base
 
+  before do
+    #you can put assignments here to apply to all pages
+  end
+
   get "/" do
+    @posts = Post.all
     erb :index #root of site is called index.
   end
    #this is the path in the URL. Like /aboout_me this / entails HP
@@ -10,10 +16,15 @@ class MyApp < Sinatra::Base
     erb :about_me
   end
 
+  # post "/" do
+  #   puts params.inspect #add input into database
+  #   redirect to ("/")
+  # end
+
   get "/tweettweet" do
     erb :twitterfeed
   end
-  
+
   get "/photos" do
     erb :instagramgrid
   end
