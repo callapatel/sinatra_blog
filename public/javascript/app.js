f7d0c524c71b1127fa33a65f8a97d3643488e37e
Calla Patel <callapatel@gmail.com>
2014-10-02 16:12:14 -0700

    changed HP

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -13,14 +13,19 @@ $( "#blog" ).mouseover(
     $( ".dropdown ").slideToggle();
   });
 
- 
-
-
   // }, function() {
   //   $( ".dropdown" ).find( "span:last" ).remove();
   // }
-
-
-
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+$(document).keydown(function(e) {
+  kkeys.push( e.keyCode );
+  if ( kkeys.toString().indexOf( konami ) >= 0 ){
+    $(document).unbind('keydown',arguments.callee);
+    $.getScript('http://www.cornify.com/js/cornify.js', function(){
+  cornify_add();
+  $(document).keydown(cornify_add);
+});
+  }
+});
 
 });
