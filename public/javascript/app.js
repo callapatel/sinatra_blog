a46b53c99f2d150be2fd3099195df5d12172a7a1
Calla Patel <callapatel@gmail.com>
2014-10-02 14:23:45 -0700

    added font for nav, other notes, JS added, drop down menu

---
+++ public/javascript/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+
+//   $( '#blog' ).hover(function () {
+//     $('.dropdown').slideToggle();
+//   });
+//
+// });
+
+//
+$( "#blog" ).mouseover(
+  function() {
+    $( ".dropdown ").slideToggle();
+  });
+
+ 
+
+
+  // }, function() {
+  //   $( ".dropdown" ).find( "span:last" ).remove();
+  // }
+
+
+
+
+});
